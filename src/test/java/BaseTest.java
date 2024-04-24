9e57c1d4ba6bce3defeb9ea25933a96617f79b17
Mustafa <mustafa.mabdelkader91@gmail.com>
2024-04-24 23:23:30 +0200

    Adding Global Properties file for Configuration Variables

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.ser.impl.PropertyBasedObjectIdGenerator;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -7,9 +8,12 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class BaseTest {
 
@@ -19,22 +23,27 @@ public class BaseTest {
 
 
     @BeforeClass
-        public void InitConfig() throws URISyntaxException, MalformedURLException {
-            //Android Driver
-            UiAutomator2Options options = new UiAutomator2Options();
-            options.setApp(System.getProperty("user.dir")+"\\src\\main\\resources\\General-Store.apk");
-            options.setDeviceName("Pixel");
+        public void InitConfig() throws URISyntaxException, IOException {
 
-            driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\resources\\data.properties");
+        prop.load(fis);
+
+        //Android Driver
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setApp(System.getProperty("user.dir")+"\\src\\main\\resources\\General-Store.apk");
+        options.setDeviceName(prop.getProperty("androidDeviceName"));
+
+        driver = new AndroidDriver(new URI("http://"+prop.getProperty("ipAddress")+":"+prop.getProperty("port")).toURL(),options);
 
 
-            //Appium Automation
-            service = new AppiumServiceBuilder()
-                    .withAppiumJS(new File("C:\\Users\\musta\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-                    .withIPAddress("127.0.0.1")
-                    .usingPort(4723)
-                    .build();
-            service.start();
+        //Appium Automation
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File(String.valueOf(prop.get("appiumJsNode"))))
+                .withIPAddress(prop.getProperty("ipAddress"))
+                .usingPort(Integer.parseInt(prop.getProperty("port")))
+                .build();
+        service.start();
         }
 
         @AfterClass
